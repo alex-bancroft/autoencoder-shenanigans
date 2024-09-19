f9e5c4828ff934ba68becacbf436edfee34b36af
Lompta <lompta@gmail.com>
2024-09-19 15:03:15 -0400

    baseline functionality!

---
+++ README.md
@@ -0,0 +1,3 @@
+Stuff you'll need (other than requirements.txt):
+* huggingface account/CLI access
+* if you want to use CUDA, CUDA on the system level and a torch implementation that's compatible (might not get that by default)
--- main.py
+++ main.py
@@ -1,21 +1,131 @@
-from transformer_lens import HookedTransformer
-from sae_lens import SAE
-from sae_lens.toolkit.pretrained_saes import get_gpt2_res_jb_saes
+from tqdm import tqdm
+from functools import partial
+from sae_lens import ActivationsStore, SAE, HookedSAETransformer
 
-# Choose a layer you want to focus on
-# For this tutorial, we're going to use layer 2
-layer = 6
+models = {
+    "gpt2-small": {
+        "release": "gpt2-small-res-jb",
+        "sae": "blocks.7.hook_resid_pre",
+    },
+    "gemma-2-2b": {
+        "release": "google/gemma-2-2b",
+        # TODO: this is wrong
+        "sae": "24-gemmascope-mlp-16k",
+    }
+}
 
-# get model
-model = HookedTransformer.from_pretrained("gemma-2b", device = device)
+model_name = "gpt2-small"
 
-# get the SAE for this layer
-sae, cfg_dict, _ = SAE.from_pretrained(
-    release = "gemma-2b-res-jb",
-    sae_id = f"blocks.{layer}.hook_resid_post",
+# Define Stuff
+device = "cuda"
+model = HookedSAETransformer.from_pretrained(model_name, device = device)
+sae, cfg_dict, sparsity = SAE.from_pretrained(
+    release = models[model_name]["release"], # <- Release name 
+    sae_id = models[model_name]["sae"], # <- SAE id (not always a hook point!)
     device = device
 )
 
-# get hook point
-hook_point = sae.cfg.hook_name
-print(hook_point)
+# a convenient way to instantiate an activation store is to use the from_sae method
+activation_store = ActivationsStore.from_sae(
+    model=model,
+    sae=sae,
+    streaming=True,
+    # fairly conservative parameters here so can use same for larger
+    # models without running out of memory.
+    store_batch_size_prompts=8,
+    train_batch_size_tokens=4096,
+    n_batches_in_buffer=32,
+    device=device,
+)
+
+def find_max_activation(model, sae, activation_store, feature_idx, num_batches=100):
+    '''
+    Find the maximum activation for a given feature index. This is useful for 
+    calibrating the right amount of the feature to add.
+    '''
+    max_activation = 0.0
+
+    pbar = tqdm(range(num_batches))
+    for _ in pbar:
+        tokens = activation_store.get_batch_tokens()
+        
+        _, cache = model.run_with_cache(
+            tokens, 
+            stop_at_layer=sae.cfg.hook_layer + 1, 
+            names_filter=[sae.cfg.hook_name]
+        )
+        sae_in = cache[sae.cfg.hook_name]
+        feature_acts = sae.encode(sae_in).squeeze()
+
+        feature_acts = feature_acts.flatten(0, 1)
+        batch_max_activation = feature_acts[:, feature_idx].max().item()
+        max_activation = max(max_activation, batch_max_activation)
+        
+        pbar.set_description(f"Max activation: {max_activation:.4f}")
+
+    return max_activation
+
+def steering(activations, hook, steering_strength=1.0, steering_vector=None, max_act=1.0):
+    # Note if the feature fires anyway, we'd be adding to that here.
+    return activations + max_act * steering_strength * steering_vector
+
+def generate_with_steering(model, sae, prompt, steering_feature, max_act, steering_strength=1.0, max_new_tokens=95):
+    input_ids = model.to_tokens(prompt, prepend_bos=sae.cfg.prepend_bos)
+    
+    steering_vector = sae.W_dec[steering_feature].to(model.cfg.device)
+    
+    steering_hook = partial(
+        steering,
+        steering_vector=steering_vector,
+        steering_strength=steering_strength,
+        max_act=max_act
+    )
+    
+    # standard transformerlens syntax for a hook context for generation
+    with model.hooks(fwd_hooks=[(sae.cfg.hook_name, steering_hook)]):
+        output = model.generate(
+            input_ids,
+            max_new_tokens=max_new_tokens,
+            temperature=0.7,
+            top_p=0.9,
+            stop_at_eos = False if device == "mps" else True,
+            prepend_bos = sae.cfg.prepend_bos,
+        )
+    
+    return model.tokenizer.decode(output[0])
+
+# Choose a feature to steer
+features = {
+    "gpt2-small": {
+        "animals": 12952,
+        "light": 17519,
+        "finances": 17522,
+    },
+    "gemma-2-2b": {
+        "pollutants": 1500,
+    }
+}
+steering_feature = steering_feature = features[model_name]["animals"]  # Choose a feature to steer towards
+
+# Find the maximum activation for this feature
+max_act = find_max_activation(model, sae, activation_store, steering_feature)
+print(f"Maximum activation for feature {steering_feature}: {max_act:.4f}")
+
+# note we could also get the max activation from Neuronpedia (https://www.neuronpedia.org/api-doc#tag/lookup/GET/api/feature/{modelId}/{layer}/{index})
+
+# Generate text without steering for comparison
+prompt = "The capital of Norway is"
+normal_text = model.generate(
+    prompt,
+    max_new_tokens=95, 
+    stop_at_eos = False if device == "mps" else True,
+    prepend_bos = sae.cfg.prepend_bos,
+)
+
+print("\nNormal text (without steering):")
+print(normal_text)
+
+# Generate text with steering
+steered_text = generate_with_steering(model, sae, prompt, steering_feature, max_act, steering_strength=2.0)
+print("Steered text:")
+print(steered_text)
